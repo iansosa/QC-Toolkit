b746d03df3fb6139324b1821edca4fb8bc50d0ce
iansosa <ian_sosa996@hotmail.com>
2024-05-02 18:42:19 +0200

    fixed compression test example

--- DFTB+MBD Framework Paper/Compression_test_UHMWPE.py
+++ DFTB+MBD Framework Paper/Compression_test_UHMWPE.py
@@ -40,55 +40,6 @@ def compressiontest_UHMWPE(vdw=None,struct_name=None,packing=[10,10,10],mbdpacki
             UHMWPE.SaveGeometry("_"+vdw+"_"+ru,"UHMWPE")
         UHMWPE.Displace_UC([0,dcomp,0])
 
-# def compressiontest_UHMWPE_10x1x1(vdw=None,struct_name=None,packing=[2,5,5],mbdpacking=[2,3,3],du=0.1):
-
-#     Nat = 5
-#     R0 = 2.4
-
-#     displ = []
-#     for i in range(0,100):
-#         u=i*du
-#         ru = str(round(u,2))
-#         if len(ru) == 4:
-#             ru = ru + '0'
-#         elif len(ru) == 3:
-#             ru = ru + '00'
-#         displ.append(ru)
-#     print(displ)
-
-#     if struct_name == None:
-#         if vdw == None:
-#             struct_name = "/UHMWPE/10x1x1/UHMWPE_2-5-5.gen"
-#         elif vdw == "MBD":
-#             struct_name = "/UHMWPE/10x1x1/UHMWPE_2-5-5_MBD233.gen"
-#         elif vdw == "TS":
-#             struct_name = "/UHMWPE/10x1x1/UHMWPE_2-5-5_TS.gen"
-
-
-#     chain = structures.Sphere(Nat,R0)
-#     chain.LoadGeometry(struct_name)
-#     Nat = chain.Nat
-#     chain.SaveGeometry()
-#     chain.RunOptimize(vdw=vdw,static=None,read_charges=False,packing=packing,mbdpacking=mbdpacking,fixlengths="XYZ")
-#     chain.LoadGeometry("geo_end.gen")
-#     if vdw == None:
-#         chain.SaveGeometry("_"+displ[0],"UHMWPE")
-#     else:
-#         chain.SaveGeometry("_"+vdw+"_"+displ[0],"UHMWPE")
-
-
-#     for i in range(1,100):
-#         chain.Displace_UC([du,0,0])
-#         chain.SaveGeometry()
-#         chain.RunOptimize(vdw=vdw,static=None,read_charges=False,packing=packing,mbdpacking=mbdpacking,fixlengths="XYZ")
-#         chain.LoadGeometry("geo_end.gen")
-#         u = i*du
-#         if vdw == None:
-#             chain.SaveGeometry("_"+displ[i],"UHMWPE")
-#         else:
-#             chain.SaveGeometry("_"+vdw+"_"+displ[i],"UHMWPE")
-
-
 def UHMWPE_comp_stats(vdw=None, packing=[10,10,10],mbdpacking=[2,2,2]):
     dcomp = -5.0/100.0
     for i in range(0,100):
